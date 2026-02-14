309317cfc544413f624dfd380c8de521ab7a4f14
Pysic <pietrornunes@hotmail.com>
2026-02-14 19:07:26 -0300

    5.1 - Criação de Tokens

---
+++ showcase-dev/tokens/colors.ts
@@ -0,0 +1,8 @@
+export const colors = {
+    background: '#000D1F',
+    primary: '#22D4FD',
+    secondary: '#123F77',
+    onBackground: '#F6F6F6',
+    onPrimary: '#123F77',
+    onSecondary: '#F6F6F6'
+};
---
+++ showcase-dev/tokens/typography.ts
@@ -0,0 +1,29 @@
+export const fontFamily = {
+    kronaOne: {
+        regular: 'KronaOne-Regular'
+    },
+    montserrat: {
+        regular: 'Montserrat-Regular',
+        semibold: 'Montserrat-SemiBold'
+    }
+}
+
+export const lineHeight = {
+    body: 1.2,
+    headings: 1.25
+};
+
+export const fontSize = {
+    body: {
+        s: 13,
+        md: 16,
+        l: 20,
+        xl: 22
+    },
+    headings: {
+        h1: 36,
+        h2: 32,
+        h3: 24,
+        h4: 20
+    }
+};
