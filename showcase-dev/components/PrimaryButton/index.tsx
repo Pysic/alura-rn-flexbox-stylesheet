0fbba7c934a639ee99cf4f407902897809c1f2f2
Pysic <pietrornunes@hotmail.com>
2026-02-14 16:41:51 -0300

    2.2 - Estilizando Botões Primários

--- showcase-dev/components/PrimaryButton/index.tsx
+++ showcase-dev/components/PrimaryButton/index.tsx
@@ -6,8 +6,8 @@ type PrimaryButtonProps = PressableProps & {
 
 export default function PrimaryButton({ title, ...rest }: PrimaryButtonProps) {
     return (
-        <Pressable {...rest}>
-            <Text>{ title }</Text>
+        <Pressable style={{backgroundColor: '#22D4FD', borderRadius: 16, padding: 23, alignItems: 'center'}} {...rest}>
+            <Text style={{color: '#000D1F', fontFamily: 'Montserrat-SemiBold', fontSize: 22, lineHeight: 24 * 1.2}} >{ title }</Text>
         </Pressable>
     )
 }
