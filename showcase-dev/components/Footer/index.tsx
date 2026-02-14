3bf11f8d899e0c68fd2ac85ebf31aa9e980151b1
Pysic <pietrornunes@hotmail.com>
2026-02-14 17:24:46 -0300

    2.4 - Estilizando Footer

--- showcase-dev/components/Footer/index.tsx
+++ showcase-dev/components/Footer/index.tsx
@@ -2,8 +2,9 @@ import { Text, View } from "react-native";
 
 export default function Footer() {
     return (
-         <View>
-            <Text style={{fontFamily: 'Montserrat-Regular', fontSize: 13, color: '#000D1F' }}>Desenvolvido por Alura. Projeto fictício sem  fins comerciais.</Text>
+        <View style={{backgroundColor: '#22D4FD', padding: 16, alignItems: 'center'}}>
+            <Text style={{fontFamily: 'Montserrat-Regular', fontSize: 13, color: '#000D1F' }}>Desenvolvido por Alura.</Text>
+            <Text style={{fontFamily: 'Montserrat-Regular', fontSize: 13, color: '#000D1F' }}>Projeto fictício sem  fins comerciais.</Text>
         </View>
     )
 }
